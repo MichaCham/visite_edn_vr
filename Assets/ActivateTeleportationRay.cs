d1c1b2ffe285e98b09ede353b7fc5e7eab383f1b
Oljdid <davidbattais.pro@gmail.com>
2024-05-14 04:32:39 +0200

    everything fianlly almost over,just need to finish roof, add some details regarding master (optionnal) and that should be it ! need also to take a look in double texturing

--- Assets/ActivateTeleportationRay.cs
+++ Assets/ActivateTeleportationRay.cs
@@ -9,11 +9,13 @@ public class ActivateTeleportationRay : MonoBehaviour
     public GameObject rightTeleportation;
     public InputActionProperty rightActivate;
     public InputActionProperty rightCancel;
-
+    public XRRayInteractor rightRay;
 
     // Update is called once per frame
     void Update()
     {
-        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
