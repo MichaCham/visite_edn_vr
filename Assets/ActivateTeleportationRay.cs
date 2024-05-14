bb6bb4da07b7bbf19719921d66c17cf0d42c57ca
Oljdid <davidbattais.pro@gmail.com>
2024-05-14 18:01:52 +0200

    V1.0 YIPPEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Assets/ActivateTeleportationRay.cs
+++ Assets/ActivateTeleportationRay.cs
@@ -9,13 +9,10 @@ public class ActivateTeleportationRay : MonoBehaviour
     public GameObject rightTeleportation;
     public InputActionProperty rightActivate;
     public InputActionProperty rightCancel;
-    public XRRayInteractor rightRay;
 
     // Update is called once per frame
     void Update()
     {
-
-        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
-        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
